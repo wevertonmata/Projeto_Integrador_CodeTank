c317c81a2de498f1b0de87ec7cac653358161c81
Leonardo Cezar <leocezar@gmail.com>
2022-07-25 20:34:11 -0300

    Update ContaPoupanca.java
    "Atualização Class Conta Poupança"

--- src/br/com/emcriptus/TiposDeContas/ContaPoupanca.java
+++ src/br/com/emcriptus/TiposDeContas/ContaPoupanca.java
@@ -1,4 +1,8 @@
 package br.com.emcriptus.TiposDeContas;
 
 public class ContaPoupanca {
+    public ContaPoupanca ((int numero, String cpf)(double saldo;)) {
+        super(numero, cpf,saldo);
+    }
+    public String diaAniversarioPoupanca (){
 }
