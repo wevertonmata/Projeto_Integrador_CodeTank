a35680e8dd15d643e787392ac676b1aef158babc
Alexandrecode <alexandrebio@outlook.com>
2022-07-26 20:58:16 -0300

    Adicionado classe ContaEspecial

---
+++ src/br/com/emcriptus/TiposDeContas/ContaEspecial.java
@@ -0,0 +1,16 @@
+package br.com.emcriptus.TiposDeContas;
+
+public class ContaEspecial extends Conta {
+    private double limite = 1000.00;
+
+    public ContaEspecial(int numero, String cpf) {
+        super(numero, cpf);
+
+    } public void usarLimite (double valor) {
+
+
+
+
+    }
+
+}
