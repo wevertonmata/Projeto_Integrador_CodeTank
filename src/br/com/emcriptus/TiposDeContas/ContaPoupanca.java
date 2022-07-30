02117a6ed89615531475b0dc42e631d3991a4e6c
Leonardo Cezar <leocezar@gmail.com>
2022-07-30 00:54:57 -0300

    Criando Aniversário da Classe Poupança

--- src/br/com/emcriptus/TiposDeContas/ContaPoupanca.java
+++ src/br/com/emcriptus/TiposDeContas/ContaPoupanca.java
@@ -1,34 +1,35 @@
 package br.com.emcriptus.TiposDeContas;
 
+import java.time.LocalDate;
+import java.time.Period;
 import java.util.Scanner;
 
-public class ContaPoupanca extends Conta{
+public class ContaPoupanca extends Conta {
 
-    private int diaAniversarioPoupanca;
-    
+    private String[] dtAniversario;
+
     public ContaPoupanca(int numero, String cpf, String nome) {
         super(numero, cpf, nome);
     }
 
     @Override
     public double debito(double valor) {
-        double saldo =  super.getSaldo() - valor;
+        double saldo = super.getSaldo() - valor;
         super.setSaldo(saldo);
-        return  saldo;
+        return saldo;
     }
 
-    public int movimento(){
-    	Scanner entrada = new Scanner(System.in);
+    public int movimento() {
+        Scanner entrada = new Scanner(System.in);
 
-        if(getSaldo() == 0){
+        if (getSaldo() == 0) {
             System.out.println("MOVIMENTO - C-Crédito:");
-        }
-        else{
+        } else {
             System.out.println("MOVIMENTO - D-debito ou C-Crédito:");
         }
 
         String movimento = entrada.nextLine().toUpperCase().trim();
-        while (!(movimento.equals("C") || movimento.equals("D"))){
+        while (!(movimento.equals("C") || movimento.equals("D"))) {
             movimento = entrada.nextLine().toUpperCase().trim();
         }
 
@@ -36,15 +37,16 @@ public class ContaPoupanca extends Conta{
         double valor = entrada.nextDouble();
         entrada.nextLine();
 
-        while (valor <= 0){
+
+        while (valor <= 0) {
             valor = entrada.nextDouble();
             entrada.nextLine();
-           
+
         }
 
         switch (movimento.toUpperCase()) {
             case ("D") -> {
-                if(getSaldo() < valor){
+                if (getSaldo() < valor) {
                     System.out.println("Valor maior que saldo atual. Não é possível efeituar o debito");
                     return 0;
                 }
@@ -58,5 +60,33 @@ public class ContaPoupanca extends Conta{
         }
         return 0;
     }
+
+    @Override
+    public void credito(double valor) {
+        Scanner entrada = new Scanner(System.in);
+        if (dtAniversario == null) {
+            System.out.println("Informe a Data de Depósito: dd/mm/aaaa");
+            dtAniversario = entrada.nextLine().split("/", 3);
+
+        } else {
+            System.out.println("Informe a Data de Depósito: dd/mm/aaaa");
+            String[] dtAtual= entrada.nextLine().split("/", 3);
+            LocalDate aniversario = LocalDate.of(Integer.parseInt(dtAniversario[2]), Integer.parseInt(dtAniversario[1]),Integer.parseInt(dtAniversario[0]));
+            LocalDate atual = LocalDate.of(Integer.parseInt(dtAtual[2]), Integer.parseInt(dtAtual[1]),Integer.parseInt(dtAtual[0]));
+            Period periodo = Period.between(aniversario, atual);
+           for (int i = 0; i <= periodo.getMonths();i++) {
+               correcao();
+            }
+            System.out.printf("Periodo: %d | Novo Saldo: R$%.2f \n", periodo.getMonths(), getSaldo());
+        }
+       double saldo = super.getSaldo() + valor;
+        super.setSaldo(saldo);
+    }
+    public void correcao() {
+        setSaldo(( getSaldo()*0.005)+getSaldo());
+
+
+    }
 }
+
 
